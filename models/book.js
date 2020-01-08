356bbb4dfb10037f7807bf752da1bfe6bb96327a
Adrienne Grace <adrienne@vandepoelproductions.com>
2020-01-07 17:17:30 -0800

    create Schemas

--- models/book.js
+++ models/book.js
@@ -1,9 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const reviewSchema = new Schema({
+    content: String,
+    rating: { type: Number, min: 1, max: 5, default: 5 }
+}, {
+    timestamps: true
+});
+
 const bookSchema = new Schema({
-    title: String,
-    author: String
+    title: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    reviews: [reviewSchema],
+    category: [{ type: Schema.Types.ObjectID, ref: 'Category' }]
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('Book', bookSchema);
