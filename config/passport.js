d1129ae88f48908758c1549f7bff1aac514cde51
Adrienne Grace <adrienne@vandepoelproductions.com>
2020-01-13 14:51:51 -0800

    remove extraneous comments

--- config/passport.js
+++ config/passport.js
@@ -8,13 +8,6 @@ passport.use(new GoodreadsStrategy({
     consumerSecret: process.env.GOODREADS_SECRET,
     callbackURL: process.env.GOODREADS_CALLBACK
 },
-/* const GOODREADS_KEY = "YBrkVG1vL9Te8NywecFtjQ"
-const GOODREADS_SECRET = "cKDob5XDH5IazBR1U2Az1rRE6bn75gIMNhxL7gY";
-passport.use(new GoodreadsStrategy({
-    consumerKey: GOODREADS_KEY,
-    consumerSecret: GOODREADS_SECRET,
-    callbackURL: "http://localhost:3000/auth/goodreads/callback"
-}, */
     function (token, tokenSecret, profile, done) {
         User.findOne({ goodreadsId: profile.id }, function (err, user) {
             if (err) return done(err);
