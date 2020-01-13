87dc32dfc38fb472ecb4449f3e05b32d041e0e63
Adrienne Grace <adrienne@vandepoelproductions.com>
2020-01-13 14:23:23 -0800

    Daniel's edits

--- controllers/books.js
+++ controllers/books.js
@@ -12,28 +12,19 @@ module.exports = {
 };
 
 function update(req, res) {
-    /* const user = User.findById(req.user._id, function (err, user) {
-       user.book.update(req.params.id);
-        user.save(function (err) {
-            res.redirect('/books');
-        }); 
-    }); */
     let book = { title, author },
         bookReview = { review, content, rating },
         bookCategory = { genre: category };
     Book.findByIdAndUpdate(req.params.id, { set: book, bookReview, bookCategory }, { new: true }, function (err, book) {
-        console.log(book.title);
-        console.log(bookCategory);
         res.redirect('/books');
     });
 }
 function editBook(req, res) {
-    Book.findById(req.params.id, function(err, book) {
+    Book.findById(req.params.id).populate('category').exec(function (err, book) {
         res.render('books/edit', { title: 'Edit Book', book, user: req.user });
     });
 }
 function deleteBook(req, res) {
-    console.log(req.params.id);
     const user = User.findById(req.user._id, function (err, user) {
         user.book.remove(req.params.id);
         user.save(function (err) {
@@ -43,13 +34,11 @@ function deleteBook(req, res) {
 }
 
 function index(req, res) {
-    /* User.findById(req.user._id).populate('book').exec(function (err, user) {
-        res.render('books/index', { title: 'My books', user });
-    }); */
-    Book.find({'_id': {$in:req.user.book}} ).populate('category').exec(function(err, books) {
-        console.log(books);
-        res.render('books/index', { title: 'My books', user: req.user});
-    })
+    User.findById(req.user._id, function (err, user) {
+        Book.find({ '_id': { $in: user.book } }).populate('category').exec(function (err, books) {
+            res.render('books/index', { title: 'My books', user: req.user, books });
+        })
+    });
 }
 
 function newBook(req, res) {
@@ -58,40 +47,32 @@ function newBook(req, res) {
 
 function create (req, res) {
     const user = User.findById(req.user._id, function(err, user){
-
-// take req.body and separate out req.body.title and req.body.author and place in separate object
-// take req.body.review and put it an a separate object, too
-// take req.body.category and put it an a 3rd object
-// create book
-// push review into review array
-// save the book document
-// create category-check to see if exists first
-    // if so doesn't, needs to be created
-    // if it does exist, associate/reference it
-// take same book document and reference the category
-// save book document again
-// redirect back to home page
     const {title, author, review, content, rating, category} = req.body;
     let book = {title, author}, 
         bookReview={review, content, rating}, 
         bookCategory={genre: category};
     const newBook = new Book(book); // this is called 'casting'
     newBook.reviews.push(bookReview);
-    console.log(bookCategory);
-        Category.findOne({genre: bookCategory}, function(err, category){
-            if (category) {
-                newBook.category = bookCategory._id;
-            } else if (!category) {
+        Category.findOne({genre: bookCategory.genre }, function(err, category){
+            if (category !== null) {
+                newBook.category = category._id;
+                newBook.save(function (err) {
+                    user.book.push(newBook._id);
+                    user.save(function () {
+                        res.redirect('/books');
+                    });
+                });
+            } else if (category === null) {
                 Category.create(bookCategory, function(err, category){
-                    newBook.category = bookCategory._id;
-                } )
+                    newBook.category = category._id;
+                    newBook.save(function (err) {
+                        user.book.push(newBook._id);
+                        user.save(function () {
+                            res.redirect('/books');
+                        });
+                    });
+                });
             }
-        })
-        newBook.save(function() {
-            user.book.push(newBook._id);
-            user.save(function(){
-                res.redirect('/books');
-            });
         })
     });
 }
