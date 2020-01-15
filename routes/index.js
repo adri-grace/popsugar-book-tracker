3488705bd621357554b8601420e5aba7eb14a3d7
Adrienne Grace <adrienne@vandepoelproductions.com>
2020-01-14 18:23:13 -0800

    updates for better UX

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ const passport = require('passport');
 router.get('/', function (req, res) {
     res.render('index', { user: req.user });
 });
-router.get('/login', function (req, res) {
+/* router.get('/login', function (req, res) {
     res.render('login', { user: req.user });
 });
-router.get('/account', ensureAuthenticated, function (req, res) {
+ router.get('/account', ensureAuthenticated, function (req, res) {
     res.render('account', { user: req.user });
-});
+}); */
 router.get('/auth/goodreads',
     passport.authenticate('goodreads'));
 
@@ -17,14 +17,14 @@ router.get('/auth/goodreads/callback',
     passport.authenticate('goodreads', {failureRedirect: '/login'}),
     function (req, res) {
         // Successful authentication, redirect home.
-        res.redirect('/');
+        res.redirect('/books');
     });
 router.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/');
 });
-function ensureAuthenticated(req, res, next) {
+/* function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) { return next(); }
     res.redirect('/login')
-}
+} */
 module.exports = router;
