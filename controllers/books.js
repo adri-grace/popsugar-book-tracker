88bd87530bfcf02d4315fe92c7e1d37cf7d7ed0a
Adrienne Grace <adrienne@vandepoelproductions.com>
2020-07-16 16:32:18 -0700

    fix edit layout and category update

--- controllers/books.js
+++ controllers/books.js
@@ -1,87 +1,93 @@
-const Book = require('../models/book');
-const Category = require('../models/category');
-const User = require('../models/user');
+const Book = require("../models/book");
+const Category = require("../models/category");
+const User = require("../models/user");
 
 module.exports = {
-    new: newBook,
-    create,
-    index,
-    deleteBook,
-    editBook,
-    update
+  new: newBook,
+  create,
+  index,
+  deleteBook,
+  editBook,
+  update,
 };
 
 function update(req, res) {
-         Book.findById(req.params.id, function(err, book) {
+  Book.findById(req.params.id, function (err, book) {
     // Book.findById(req.params.id).populate('category').exec(function (err, book) {
-                book.title = req.body.title;
-                book.author = req.body.author;
-                book.reviews[0].rating = req.body.rating;
-                book.reviews[0].content = req.body.content;
-                book.category.genre = req.body.category;
-                console.log(req.body);
-                console.log(book.category._id);
-                book.save(function(err, book) {
-                    res.redirect('/books');
-                })
-            });
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.reviews[0].rating = req.body.rating;
+    book.reviews[0].content = req.body.content;
+    book.category.genre = req.body.category.genre;
+    console.log(req.body);
+    console.log(book.category._id);
+    book.save(function (err, book) {
+      res.redirect("/books");
+    });
+  });
 }
 function editBook(req, res) {
-    Book.findById(req.params.id).populate('category').exec(function (err, book) {
-        res.render('books/edit', { title: 'Edit Book', book, user: req.user });
+  Book.findById(req.params.id)
+    .populate("category")
+    .exec(function (err, book) {
+      res.render("books/edit", { title: "Edit Book", book, user: req.user });
     });
 }
 function deleteBook(req, res) {
-    const user = User.findById(req.user._id, function (err, user) {
-        user.book.remove(req.params.id);
-        user.save(function (err) {
-            res.redirect('/books');
-        });
+  const user = User.findById(req.user._id, function (err, user) {
+    user.book.remove(req.params.id);
+    user.save(function (err) {
+      res.redirect("/books");
     });
+  });
 }
 
 function index(req, res) {
-    User.findById(req.user._id, function (err, user) {
-        Book.find({ '_id': { $in: user.book } }).populate('category').exec(function (err, books) {
-            res.render('books/index', { 
-                title: 'My books', 
-                user: req.user, books });
+  User.findById(req.user._id, function (err, user) {
+    Book.find({ _id: { $in: user.book } })
+      .populate("category")
+      .exec(function (err, books) {
+        res.render("books/index", {
+          title: "My books",
+          user: req.user,
+          books,
         });
-    });
+      });
+  });
 }
 
 function newBook(req, res) {
-    res.render('books/new', { title: 'Add a Book', user: req.user });
+  res.render("books/new", { title: "Add a Book", user: req.user });
 }
 
-function create (req, res) {
-    const user = User.findById(req.user._id, function(err, user){
-    const {title, author, review, content, rating, category} = req.body;
-    let book = {title, author}, 
-        bookReview={review, content, rating}, 
-        bookCategory={genre: category};
+function create(req, res) {
+  const user = User.findById(req.user._id, function (err, user) {
+    const { title, author, review, content, rating, category } = req.body;
+    let book = { title, author },
+      bookReview = { review, content, rating },
+      bookCategory = { genre: category };
     const newBook = new Book(book); // this is called 'casting'
     newBook.reviews.push(bookReview);
-        Category.findOne({genre: bookCategory.genre }, function(err, category){
-            if (category !== null) {
-                newBook.category = category._id;
-                newBook.save(function (err) {
-                    user.book.push(newBook._id);
-                    user.save(function () {
-                        res.redirect('/books');
-                    });
-                });
-            } else if (category === null) {
-                Category.create(bookCategory, function(err, category){
-                    newBook.category = category._id;
-                    newBook.save(function (err) {
-                        user.book.push(newBook._id);
-                        user.save(function () {
-                            res.redirect('/books');
-                        });
-                    });
-                });
-            }
+    Category.findOne({ genre: bookCategory.genre }, function (err, category) {
+      if (category !== null) {
+        newBook.category = category._id;
+        newBook.save(function (err) {
+          user.book.push(newBook._id);
+          user.save(function () {
+            res.redirect("/books");
+          });
         });
+      } else if (category === null) {
+        Category.create(bookCategory, function (err, category) {
+          newBook.category = category._id;
+          newBook.save(function (err) {
+            user.book.push(newBook._id);
+            user.save(function () {
+              res.redirect("/books");
+            });
+          });
+        });
+      }
     });
+  });
 }
